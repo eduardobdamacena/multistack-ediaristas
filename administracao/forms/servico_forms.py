faea0ee8ab72d1c81ecdab35280e6882cbb50e85
Eduardo Busato Damacena <eduardobusato51@gmail.com>
2021-08-05 22:02:04 -0300

    Cadastro de usuários finalizado

--- administracao/forms/servico_forms.py
+++ administracao/forms/servico_forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.db.models import fields
-from .models import Servico
+from ..models import Servico
 from decimal import Decimal
 
 class ServicoForm(forms.ModelForm):
