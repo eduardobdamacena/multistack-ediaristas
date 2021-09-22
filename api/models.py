d380d08afe254600bf35cefb656448d21198c9be
Eduardo Busato Damacena <eduardobusato51@gmail.com>
2021-09-21 22:23:43 -0300

    Obtendo o valor da comissão de serviços

--- api/models.py
+++ api/models.py
@@ -61,8 +61,8 @@ class Diaria(models.Model):
     data_atendimento = models.DateTimeField(null=False, blank=False)
     tempo_atendimento = models.IntegerField(null=False, blank=False)
     status = models.IntegerField(null=False, blank=False, choices=STATUS_DIARIA_CHOICES, default=1)
-    preco = models.FloatField(null=False, blank=False)
-    valor_comissao = models.FloatField(null=False, blank=False)
+    preco = models.DecimalField(null=False, blank=False, decimal_places=2, max_digits=5)
+    valor_comissao = models.DecimalField(null=False, blank=False, decimal_places=2, max_digits=5)
     logradouro = models.CharField(max_length=60, null=False, blank=False)
     numero = models.CharField(max_length=10, null=False, blank=False)
     bairro = models.CharField(max_length=30, null=False, blank=False)
@@ -84,7 +84,7 @@ class Diaria(models.Model):
     diarista = models.ForeignKey(Usuario, null=True, blank=True, on_delete=models.DO_NOTHING,
         related_name='diarista')
     servico = models.ForeignKey(Servico, null=False, blank=False, on_delete=models.DO_NOTHING)
-    candidatos = models.ManyToManyField(Usuario, blank=True, related_name='candidatos')
+    candidatas = models.ManyToManyField(Usuario, blank=True, related_name='candidatas')
     created_at = models.DateField(auto_now_add=True, null=True)
     updated_at = models.DateField(auto_now=True, null=True)
 
